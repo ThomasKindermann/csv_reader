f264cb433a314f008163791f7961dfbab04b46f9
ThomasKindermann <kindermann@kitconcept.com>
2021-06-08 10:23:01 +0200

    first commit

---
+++ csv_reader/csv_reader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace csv_reader
+{
+    public class csv_reader : csv_reader_interface
+    {
+
+        private string[] line;
+        public string[] readFile(string path)
+        {
+            using (StreamReader sr = new StreamReader("TestFile.txt"))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            return null;
+        }
+
+        
+
+
+
+
+
+    }
+}
---
+++ csv_reader/csv_reader_interface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace csv_reader
+{
+    public interface csv_reader_interface
+    {
+
+        string[] readFile(string path);
+
+
+    }
+}
